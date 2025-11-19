c863f82ccfecc91267a6c0fd0d3b1d1141b47a17
hina123456677 <hbatool551@gmail.com>
2025-11-19 12:06:58 +0500

    docker

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 8000;
-  await app.listen(process.env.PORT ?? 8000);
+
+  // Listen on all network interfaces, not just localhost
+  await app.listen(port, '0.0.0.0');
+
   Logger.log(`Server is running on port ${port}`);
 }
 bootstrap();
